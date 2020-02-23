7dad074332a3d5cf1b29c1b044413f07ada27669
louiewhw <louiewhw@gmail.com>
2020-02-23 14:12:10 -0800

    added country csv

--- VA7/assets/js/graph.js
+++ VA7/assets/js/graph.js
@@ -3,3 +3,16 @@ Plotly.newPlot( TESTER, [{
 x: [1, 2, 3, 4, 5],
 y: [1, 2, 4, 8, 16] }], {
 margin: { t: 0 } } );
+
+d3.csv("/country.csv", function(data) {
+    console.log('bye');
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        console.log(data[i]);
+        console.log('Hi');
+    }
+});
+
+
+console.log('Hi');

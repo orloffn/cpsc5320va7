48334f7253fd65d107f9cb9484d773991c4ce7a2
orloff <n.orloff@f5.com>
2020-02-24 10:30:47 -0800

    map and scatterplot

--- VA7/assets/js/graph.js
+++ VA7/assets/js/graph.js
@@ -27,16 +27,34 @@ Plotly.d3.csv('assets/js/country.csv', function(err, rows){
     // tone visual
 
     // scatterplot
-    var data = {
+	// function onlyUnique(value, index, self) { 
+	// 	return self.indexOf(value) === index;
+	// }
+	// var countries = unpack(rows, 'country').filter(onlyUnique)
+	// var colors = []
+	// for (var i = countries.length - 1; i >= 0; i--) {
+	// 	colors.push('rgb(255, 112, ' + (12 + i) + ')')
+	// }
+	// var marker_cols = {}
+	// countries.forEach((key, i) => marker_cols[key] = colors[i]);
+	// var colors_mapped = [for (x of unpack(rows, 'country')) marker_cols[x]]
+	// console.log(colors_mapped)
+	var data = [{
 		x: unpack(rows, 'audio_length'),
 		y: unpack(rows, 'db'),
-		mode: 'lines+markers',
-		type: 'scatter'
-	};
+		text: unpack(rows, 'country'),
+		mode: 'markers',
+		marker: {
+			// color: colors_mapped,
+			opacity: 0.5,
+			size: 5,
+			line: {
+				color: 'rgb(0, 0, 0)',
+				width: 1
+			}
+		},
+		showlegend: false
+	}];
 
 	Plotly.newPlot('graph3', data)
 });
-
-// tone visual
-
-// scatterplot

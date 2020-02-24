138858452dfdd80ca21e56f9481860740f4fc29c
orloff <n.orloff@f5.com>
2020-02-23 18:15:02 -0800

    Started working on plotly visuals

--- VA7/assets/js/graph.js
+++ VA7/assets/js/graph.js
@@ -1,18 +1,42 @@
-TESTER = document.getElementById('tester');
-Plotly.newPlot( TESTER, [{
-x: [1, 2, 3, 4, 5],
-y: [1, 2, 4, 8, 16] }], {
-margin: { t: 0 } } );
-
-d3.csv("/country.csv", function(data) {
-    console.log('bye');
-    console.log(data);
-    for (var i = 0; i < data.length; i++) {
-        console.log(data[i]);
-        console.log(data[i]);
-        console.log('Hi');
+Plotly.d3.csv('assets/js/country.csv', function(err, rows){
+	// map visual
+    function unpack(rows, key) {
+        return rows.map(function(row) { return row[key]; });
     }
+
+    var data = [{
+        type: 'choropleth',
+        locationmode: 'country names',
+        locations: unpack(rows, 'country'),
+        z: unpack(rows, 'audio_length'),
+        text: unpack(rows, 'country'),
+        autocolorscale: true
+    }];
+
+    var layout = {
+      title: '',
+      geo: {
+          projection: {
+              type: 'robinson'
+          }
+      }
+    };
+
+    Plotly.newPlot("graph1", data, layout, {showLink: false});
+
+    // tone visual
+
+    // scatterplot
+    var data = {
+		x: unpack(rows, 'audio_length'),
+		y: unpack(rows, 'db'),
+		mode: 'lines+markers',
+		type: 'scatter'
+	};
+
+	Plotly.newPlot('graph3', data)
 });
 
+// tone visual
 
-console.log('Hi');
+// scatterplot
